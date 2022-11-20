ef8b9187024f488316ba39d1c1d2fbb9074e8fbb
Michael Talalaev <talalaev.misha@gmail.com>
2022-11-20 18:45:43 +0400

    Initial commit

---
+++ SameCase.js
@@ -0,0 +1,17 @@
+
+
+function sameCaseNew(a, b){
+    if (!a.match(/[a-z]/i) || !b.match(/[a-z]/i)){
+        return -1
+    }
+    if (a === a.toUpperCase()&& b === b.toUpperCase()){
+        return 1
+    }
+    if (a === a.toLowerCase() && b === b.toLowerCase()){
+        return 1
+    }else{
+        return 0
+    }
+
+}
+
